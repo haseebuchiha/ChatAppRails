870783c12c6cd92eb7fba1d8747024836acd3c8c
Haseeb Arshad <haseeb9009@gmail.com>
2020-12-11 01:05:15 +0500

    complete login and showing chat when button is clicked with text

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Message.create(body: "Tum sunao kya haal chaal ha", user: User.last)
+Message.create(body: "mein kya sunao haal chaal", user: User.first)
+Message.create(body: "Tu to ha hi bghairat pasiay de mere ", user: User.last)
+Message.create(body: "Yaar ajeeb ab mein piyo laga hua hu paisay du", user: User.first)
+Message.create(body: "Tum na baaz ana saein kabhi bhi.. Allah ko hisab dena ab", user: User.first)
+Message.create(body: "Yaar ye dhamkiyan na lagaya karo de raha hu na ", user: User.last)
+Message.create(body: "Sai ha bhai sai ha ", user: User.first)
